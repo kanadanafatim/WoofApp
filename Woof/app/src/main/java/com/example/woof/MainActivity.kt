15cf690f4449a68aeb0bff103695800c249d031f
yeo fatim <aalyahfatim@gmail.com>
2025-01-25 17:19:58 +0000

    maj

--- Woof/app/src/main/java/com/example/woof/MainActivity.kt
+++ Woof/app/src/main/java/com/example/woof/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -63,59 +66,86 @@ fun WoofApp() {
 
         // Liste des chiens
         val dogList = listOf(
-            Dog("Koda", "2 years old", R.drawable.koda, "Editing treats on the terrace "),
-            Dog("Lola", "16 years old", R.drawable.lola, "Barking at Daddy"),
-            Dog("Frankie", "2 years old", R.drawable.frankie, ""),
-            Dog("Nox", "8 years old", R.drawable.nox, ""),
-            Dog("Faye", "8 years old", R.drawable.faye, ""),
-            Dog("Bella", "14 years old", R.drawable.bella, "")
+            Dog("Koda", "2 years old", "About", R.drawable.koda, "Editing treats on the terrace"),
+            Dog("Lola", "16 years old", "About", R.drawable.lola, "Barking at Daddy"),
+            Dog("Frankie", "2 years old", "", R.drawable.frankie, ""),
+            Dog("Nox", "8 years old", "", R.drawable.nox, ""),
+            Dog("Faye", "8 years old", "", R.drawable.faye, ""),
+            Dog("Bella", "14 years old", "", R.drawable.bella, "")
         )
 
-        LazyColumn {
-            items(dogList) { dog ->
-                DogItem(dog = dog)
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(8.dp) // Ajouter un espacement entre les éléments
+        ) {
+            itemsIndexed(dogList) { index, dog ->
+                DogItem(dog = dog, index = index)
             }
         }
     }
 }
 
 @Composable
-fun DogItem(dog: Dog) {
-    Row(
+fun DogItem(dog: Dog, index: Int) {
+    val cardHeight = if (index < 2) 140.dp else 100.dp
+    Card(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically
+
+            .padding(horizontal = 16.dp, vertical = 4.dp)  
+            .height(cardHeight),
+        colors = CardDefaults.cardColors(containerColor = Color.Gray.copy(alpha = 0.2f)) 
     ) {
-        Image(
-            painter = painterResource(id = dog.imageRes),
-            contentDescription = null,
-            modifier = Modifier.size(50.dp)
-        )
-        Spacer(modifier = Modifier.width(16.dp))
-        Column {
-            Text(
-                text = dog.name,
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp), // Ajuster le padding pour aligner les éléments
+            verticalAlignment = Alignment.Top, // Aligner les éléments en haut
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+            ) {
+                Image(
+                    painter = painterResource(id = dog.imageRes),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+            Spacer(modifier = Modifier.width(16.dp)) // Ajouter un espace entre l'image et le texte
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = dog.name,
+                    fontFamily = FontFamily(Font(R.font.montserrat_bold)),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+
+                Text(
+                    text = dog.age,
+                    fontFamily = FontFamily(Font(R.font.montserrat_regular)),
+                    fontStyle = FontStyle.Italic,
+                    fontSize = 12.sp
+                )
+                Text(
+                        text = dog.about2,
                 fontFamily = FontFamily(Font(R.font.montserrat_bold)),
                 fontWeight = FontWeight.Bold,
-                fontSize = 16.sp
-            )
-            Text(
-                text = dog.distance,
-                fontFamily = FontFamily(Font(R.font.montserrat_regular)),
-                fontStyle = FontStyle.Italic,
                 fontSize = 12.sp
-            )
-            Text(
-                text = dog.about,
-                fontFamily = FontFamily(Font(R.font.montserrat_regular)),
-                fontSize = 14.sp
-            )
+                )
+                Text(
+                    text = dog.about,
+                    fontFamily = FontFamily(Font(R.font.montserrat_regular)),
+                    fontSize = 12.sp
+                )
+            }
         }
     }
 }
 
-data class Dog(val name: String, val distance: String, val imageRes: Int, val about: String)
+data class Dog(val name: String, val age: String, val about2: String, val imageRes: Int, val about: String)
 
 @Preview(showBackground = true)
 @Composable
